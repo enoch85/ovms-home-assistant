fa875df16573df8145afbf6dcf0142e343d59bcc
Daniel Hansson <mailto@danielhansson.nu>
2025-03-02 21:36:16 +0100

    Update __init__.py
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- custom_components/ovms_mqtt/__init__.py
+++ custom_components/ovms_mqtt/__init__.py
@@ -1,24 +1,35 @@
-import logging
-from homeassistant.core import HomeAssistant
+"""The Open Vehicle Monitoring System (OVMS) integration."""
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 
-_LOGGER = logging.getLogger(__name__)
 
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the OVMS MQTT integration."""
-    _LOGGER.debug("Setting up OVMS MQTT integration")
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
+    """Set up the OVMS MQTT component."""
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up OVMS MQTT from a config entry."""
-    _LOGGER.debug("Setting up OVMS MQTT config entry")
-    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    # Forward the setup to the sensor platform.
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    _LOGGER.debug("Unloading OVMS MQTT config entry")
+    # Forward the unload to the sensor platform.
     await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+
+    # Clean up
+    hass.data[DOMAIN].pop(entry.entry_id)
+    if not hass.data[DOMAIN]:
+        hass.data.pop(DOMAIN)
+
     return True
