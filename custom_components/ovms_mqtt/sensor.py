05fc425c3db4fae385c683c1858f223164c38825
Daniel Hansson <mailto@danielhansson.nu>
2025-03-02 19:14:37 +0100

    Update sensor.py
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- custom_components/ovms_mqtt/sensor.py
+++ custom_components/ovms_mqtt/sensor.py
@@ -1,80 +1,21 @@
-"""Sensor platform for OVMS MQTT integration."""
-import logging
+from homeassistant.helpers.entity import Entity
+from homeassistant.const import STATE_UNKNOWN
 
-from homeassistant.components.mqtt import async_subscribe
-from homeassistant.components.sensor import SensorEntity
-from homeassistant.core import callback
-
-from .const import DOMAIN
-
-_LOGGER = logging.getLogger(__name__)
-
-
-async def async_setup_entry(hass, config_entry, async_add_entities):
-    """Set up the OVMS MQTT sensor platform."""
-    config = config_entry.data
-    topic_prefix = config.get("topic_prefix", "ovms")
-    qos = config.get("qos", 1)
-
-    _LOGGER.debug("Setting up OVMS MQTT sensor platform with config: %s", config)
-
-    @callback
-    def message_received(msg):
-        """Handle new MQTT messages."""
-        _LOGGER.debug("Received message: %s %s", msg.topic, msg.payload)
-
-        # Extract user, vehicle_id, and topic type (metric/notify/client) from the topic
-        parts = msg.topic.split("/")
-        _LOGGER.debug("Topic parts: %s", parts)  # Log the topic parts
-
-        if len(parts) >= 4:  # ovms/user/vehicle_id/[metric|notify|client]/...
-            user = parts[1]
-            vehicle_id = parts[2]
-            topic_type = parts[3]  # metric, notify, or client
-            metric_path = "/".join(parts[4:])  # Everything after the topic type
-
-            # Create a unique sensor ID and name
-            sensor_id = f"ovms_{user}_{vehicle_id}_{topic_type}_{metric_path.replace('/', '_')}"
-            sensor_name = f"OVMS {vehicle_id} {topic_type} {metric_path.replace('/', ' ')}"
-
-            _LOGGER.debug("Creating sensor: %s (%s)", sensor_name, sensor_id)  # Log sensor creation
-
-            # Create or update the sensor
-            async_add_entities([
-                OVMSMQTTSensor(sensor_id, sensor_name, msg.topic, msg.payload)
-            ])
-        else:
-            _LOGGER.debug("Invalid topic structure: %s", msg.topic)  # Log invalid topics
-
-    # Subscribe to all relevant topics using wildcards
-    topics = [
-        f"{topic_prefix}/+/+/metric/#",
-        f"{topic_prefix}/+/+/notify/#",
-        f"{topic_prefix}/+/+/client/#",
-    ]
-
-    for topic in topics:
-        _LOGGER.debug("Subscribing to MQTT topic: %s with QoS: %s", topic, qos)
-        await async_subscribe(
-            hass,
-            topic,
-            message_received,
-            qos=qos,
-        )
-    _LOGGER.debug("Successfully subscribed to MQTT topics")
-
-
-class OVMSMQTTSensor(SensorEntity):
+class OvmsSensor(Entity):
     """Representation of an OVMS MQTT sensor."""
 
-    def __init__(self, sensor_id, name, topic, payload):
+    def __init__(self, vin: str, metric_key: str, value):
         """Initialize the sensor."""
-        _LOGGER.debug("Initializing sensor: %s (%s)", name, sensor_id)
-        self._sensor_id = sensor_id
-        self._name = name
-        self._topic = topic
-        self._payload = payload
-        self._state = None
+        self._vin = vin
+        self._metric_key = metric_key
+        self._value = value
+        self._unique_id = f"{vin}_{metric_key.replace('/', '_')}"
+        self._name = f"OVMS {vin} {metric_key.replace('/', ' ')}"
+
+    @property
+    def unique_id(self):
+        """Return a unique ID for the sensor."""
+        return self._unique_id
 
     @property
     def name(self):
@@ -84,15 +25,14 @@ class OVMSMQTTSensor(SensorEntity):
     @property
     def state(self):
         """Return the state of the sensor."""
-        return self._state
+        return self._value
 
     @property
-    def unique_id(self):
-        """Return a unique ID for the sensor."""
-        return self._sensor_id
+    def should_poll(self):
+        """No polling needed for MQTT sensors."""
+        return False
 
-    @callback
-    def async_update(self):
-        """Update the sensor state."""
-        _LOGGER.debug("Updating sensor state: %s", self._payload)
-        self._state = self._payload
+    def update_value(self, value):
+        """Update the sensor's value."""
+        self._value = value
+        self.schedule_update_ha_state()
