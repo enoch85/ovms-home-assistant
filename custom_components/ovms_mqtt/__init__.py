7b90885de40c81fc5d321e57a62e212a5acd5947
Daniel Hansson <mailto@danielhansson.nu>
2025-03-02 13:17:38 +0100

    more fixes
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- custom_components/ovms_mqtt/__init__.py
+++ custom_components/ovms_mqtt/__init__.py
@@ -3,10 +3,14 @@ import logging
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
+from homeassistant.helpers import config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "ovms_mqtt"
+
+# Define a config schema (empty since this integration is configured via MQTT)
+CONFIG_SCHEMA = cv.empty_config_schema(DOMAIN)
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
