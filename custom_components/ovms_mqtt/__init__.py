2108c6f79ce817840480910b5603ff2fbd5061bb
Daniel Hansson <mailto@danielhansson.nu>
2025-03-02 16:01:04 +0100

    Update __init__.py
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- custom_components/ovms_mqtt/__init__.py
+++ custom_components/ovms_mqtt/__init__.py
@@ -3,11 +3,11 @@ import logging
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.components.mqtt import async_subscribe
 
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
+
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up OVMS MQTT from a config entry."""
@@ -21,6 +21,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.config_entries.async_forward_entry_setup(entry, "sensor")
     )
     return True
+
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
