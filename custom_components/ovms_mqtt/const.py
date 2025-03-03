2ef04ae956b30cc697b6e9c6502fc724318a6ee0
Daniel Hansson <mailto@danielhansson.nu>
2025-03-04 00:26:24 +0100

    Update const.py
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- custom_components/ovms_mqtt/const.py
+++ custom_components/ovms_mqtt/const.py
@@ -1,7 +1,12 @@
+"""Constants for the OVMS MQTT integration."""
+
 DOMAIN = "ovms_mqtt"
+
+# Configuration constants
 CONF_BROKER = "broker"
 CONF_PORT = "port"
 CONF_USERNAME = "username"
 CONF_PASSWORD = "password"
 CONF_TOPIC_PREFIX = "topic_prefix"
 CONF_QOS = "qos"
+CONF_DEBUG_LOGGING = "debug_logging"
