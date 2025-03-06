498bd1b8afc39adfec6eb25b56eb6d919cd63be4
Daniel Hansson <mailto@danielhansson.nu>
2025-03-06 17:19:42 +0100

    Update __init__.py
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,16 @@
 """Tests for OVMS integration."""
+import pytest
+from unittest.mock import patch, MagicMock
+from homeassistant.core import HomeAssistant
+from homeassistant.setup import async_setup_component
+from custom_components.ovms.const import DOMAIN
+from tests.const import (
+    UnitOfTemperature, 
+    UnitOfLength, 
+    UnitOfSpeed,
+    UnitOfElectricCurrent,
+    UnitOfElectricPotential,
+    UnitOfEnergy,
+    UnitOfPower,
+    UnitOfPressure
+)
