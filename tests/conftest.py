25bdb8a03c809739eb794612978fe32b9bc0ca4f
Daniel Hansson <mailto@danielhansson.nu>
2025-03-06 17:29:04 +0100

    Update conftest.py
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,6 @@
 import pytest
 from unittest.mock import patch
 
-pytest.register_assert_rewrite("tests.common")
-
 # This fixture is used to prevent HomeAssistant from trying to create .homeassistant directories
 @pytest.fixture(autouse=True)
 def auto_enable_custom_integrations(enable_custom_integrations):
