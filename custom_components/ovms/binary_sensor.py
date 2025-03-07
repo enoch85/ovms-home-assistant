abe9e0e5f58e74aa9af194ccd705651458ebd2b1
Daniel Hansson <mailto@danielhansson.nu>
2025-03-07 21:01:58 +0100

    Update binary_sensor.py
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- custom_components/ovms/binary_sensor.py
+++ custom_components/ovms/binary_sensor.py
@@ -184,6 +184,7 @@ class OVMSBinarySensor(BinarySensorEntity, RestoreEntity):
         # Default value
         self._attr_device_class = None
         self._attr_icon = None
+        self._attr_entity_category = None
         
         # Try to find matching metric by converting topic to dot notation
         topic_suffix = self._topic
@@ -212,6 +213,8 @@ class OVMSBinarySensor(BinarySensorEntity, RestoreEntity):
                 self._attr_device_class = metric_info["device_class"]
             if "icon" in metric_info:
                 self._attr_icon = metric_info["icon"]
+            if "entity_category" in metric_info:
+                self._attr_entity_category = metric_info["entity_category"]
             return
         
         # If no metric info was found, use the original pattern matching as fallback
