b6bd976047960c42f8aea99642191997aadd56da
Daniel Hansson <mailto@danielhansson.nu>
2025-03-13 15:24:42 +0100

    use naming class
    Signed-off-by: Daniel Hansson <mailto@danielhansson.nu>

--- custom_components/ovms/binary_sensor.py
+++ custom_components/ovms/binary_sensor.py
@@ -112,8 +112,13 @@ class OVMSBinarySensor(BinarySensorEntity, RestoreEntity):
         self._attr_unique_id = unique_id
         # Use the entity_id compatible name for internal use
         self._internal_name = name
-        # Set the entity name that will display in UI to friendly name or name
-        self._attr_name = friendly_name or name.replace("_", " ").title()
+        
+        # Set the entity name that will display in UI - ALWAYS use friendly_name when provided
+        if friendly_name:
+            self._attr_name = friendly_name
+        else:
+            self._attr_name = name.replace("_", " ").title()
+            
         self._topic = topic
         self._attr_is_on = self._parse_state(initial_state)
         self._attr_device_info = device_info
